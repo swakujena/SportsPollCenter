755c3290034e1c080d1503abc5ab5d5f077b1e2a
swakujena <swakujena@gmail.com>
2019-11-07 16:39:36 +0100

    2 files modified

--- Home.js
+++ Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 
+
 class Home extends Component {
 	render() {
 		return (
         <div>
-          <h1>Vote & Win</h1>
+          <h1><marquee behavior="alternate"> VOTE & WIN</marquee></h1>
         </div>
        )
     }
